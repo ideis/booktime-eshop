55b84982f7b8a145f358e8175154d704fc2a0e53
Ilya Kontaev <ideis@yandex.ru>
2019-09-08 11:14:47 +0300

    add signup view

--- main/views.py
+++ main/views.py
@@ -1,9 +1,15 @@
+
 from django.views.generic.edit import FormView
 from django.views.generic.list import ListView
 from django.shortcuts import get_object_or_404
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 from main import forms
 from main import models
+
+import logging
+logger = logging.getLogger(__name__)
 
 
 class ContactUsView(FormView):
@@ -34,3 +40,29 @@ class ProductListView(ListView):
         else:
             products = models.Product.objects.active()
         return products.order_by("name")
+
+
+class SignupView(FormView):
+    template_name = "signup.html"
+    form_class = forms.UserCreationForm
+
+    def get_success_url(self):
+        redirect_to = self.request.GET.get("next", "/")
+        return redirect_to
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        form.save()
+        
+        email = form.cleaned_data.get("email")
+        raw_password = form.cleaned_data.get("password1")
+        logger.info(
+            "New signup for email=%s through SignupView", email
+        )
+        user = authenticate(email=email, password=raw_password)
+        login(self.request, user)
+        form.send_mail()
+        messages.info(
+            self.request, "You signed up successfully."
+        )
+        return response

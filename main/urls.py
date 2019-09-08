ecd8ce39d52475623c7dd2f64f281eee46741fdc
Ilya Kontaev <ideis@yandex.ru>
2019-09-08 13:42:21 +0300

    add Address CRUD routes

--- main/urls.py
+++ main/urls.py
@@ -19,9 +19,37 @@ urlpatterns = [
         name="about_us",
     ),
     path(
+        "address/",
+        views.AddressListView.as_view(),
+        name="address_list",
+    ),
+    path(
+       "address/create/",
+        views.AddressCreateView.as_view(),
+        name="address_create",
+    ),
+    path(
+       "address/<int:pk>/",
+       views.AddressUpdateView.as_view(),
+       name="address_update",
+    ),
+    path(
+       "address/<int:pk>/delete/",
+       views.AddressDeleteView.as_view(),
+       name="address_delete",
+    ),
+    path(
         "contact-us/",
         views.ContactUsView.as_view(template_name="contact_form.html"),
         name="contact_us"
+    ),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(
+            template_name="login.html",
+            form_class=forms.AuthenticationForm,
+        ),
+        name="login",
     ),
     path(
         "products/<slug:tag>/",
@@ -37,13 +65,5 @@ urlpatterns = [
         "signup/",
         views.SignupView.as_view(),
         name="signup"
-    ),
-    path(
-        "login/",
-        auth_views.LoginView.as_view(
-            template_name="login.html",
-            form_class=forms.AuthenticationForm,
-        ),
-        name="login",
     ),
 ]
